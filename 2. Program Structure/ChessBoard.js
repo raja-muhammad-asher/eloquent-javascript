dd7507e9a3792c10873a283a0bf8bdef719456c6
Raja Muhammad Asher <raja.muhammad.asher@gmail.com>
2023-01-23 22:42:21 +0500

    Update ChessBoard.js

--- 2. Program Structure/ChessBoard.js
+++ 2. Program Structure/ChessBoard.js
@@ -1,1 +1,16 @@
+const size = 8;
 
+let chessBoard = "";
+
+for (let i = 0; i < size; i++) {
+  for (let j = 0; j < size; j++) {
+    if ((i + j) % 2 == 0) {
+      chessBoard += " ";
+    } else {
+      chessBoard += "#";
+    }
+  }
+  chessBoard += "\n";
+}
+
+console.log(chessBoard);

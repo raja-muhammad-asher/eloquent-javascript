df7fb972aa6c996f3b7086e9eca4af773bb54960
Raja Muhammad Asher <raja.muhammad.asher@gmail.com>
2023-03-13 15:18:37 +0500

    Update everything.js

--- 5. Higher Order Functions/everything.js
+++ 5. Higher Order Functions/everything.js
@@ -1,1 +1,10 @@
+function every(array, predicate) {
+  for (let element of array) {
+    if (!predicate(element)) return false;
+  }
+  return true;
+}
 
+function every2(array, predicate) {
+  return !array.some(element => !predicate(element));
+}

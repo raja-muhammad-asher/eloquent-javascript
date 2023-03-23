e975441f3c451f9c9755cb1b8ef572f72e370974
Raja Muhammad Asher <raja.muhammad.asher@gmail.com>
2023-03-23 16:19:24 +0500

    Update vector.js

--- 6. The Secret Life of Objects/vector.js
+++ 6. The Secret Life of Objects/vector.js
@@ -1,1 +1,18 @@
+class Vec {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 
+  plus(other) {
+    return new Vec(this.x + other.x, this.y + other.y);
+  }
+
+  minus(other) {
+    return new Vec(this.x - other.x, this.y - other.y);
+  }
+
+  get length() {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
+}

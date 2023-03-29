26d6b5869e60f7311bf5e93ad792a0edeefdb7e7
Raja Muhammad Asher <raja.muhammad.asher@gmail.com>
2023-03-29 23:22:48 +0500

    Create groups.js

---
+++ 6. The Secret Life of Objects/groups.js
@@ -0,0 +1,27 @@
+class Group {
+  constructor() {
+    this.members = [];
+  }
+
+  add(value) {
+    if (!this.has(value)) {
+      this.members.push(value);
+    }
+  }
+
+  delete(value) {
+    this.members = this.members.filter(v => v !== value);
+  }
+
+  has(value) {
+    return this.members.includes(value);
+  }
+
+  static from(collection) {
+    let group = new Group;
+    for (let value of collection) {
+      group.add(value);
+    }
+    return group;
+  }
+}

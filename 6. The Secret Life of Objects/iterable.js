30c2bf1ee7219b54f86e4d253ca146eb0f14f114
Raja Muhammad Asher <raja.muhammad.asher@gmail.com>
2023-03-31 16:28:13 +0500

    Update iterable.js

--- 6. The Secret Life of Objects/iterable.js
+++ 6. The Secret Life of Objects/iterable.js
@@ -1,3 +1,19 @@
+/* Iterable groups
+Make the Group class from the previous exercise iterable. Refer to the section about the iterator interface earlier in the chapter if you aren’t clear on the exact form of the interface anymore.
+
+If you used an array to represent the group’s members, don’t just return the iterator created by calling the Symbol.iterator method on the array. That would work, but it defeats the purpose of this exercise.
+
+It is okay if your iterator behaves strangely when the group is modified during iteration.
+
+// Your code here (and the code from the previous exercise)
+
+for (let value of Group.from(["a", "b", "c"])) {
+  console.log(value);
+}
+// → a
+// → b
+// → c */
+
 class Group {
   constructor() {
     this.members = [];

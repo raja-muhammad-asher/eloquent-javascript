752c7931d93a4925a52cd46b4fb11bb1280d23e4
Raja Muhammad Asher <raja.muhammad.asher@gmail.com>
2023-04-05 22:57:42 +0500

    Update efficiency.js

--- 7. Project: A Robot/efficiency.js
+++ 7. Project: A Robot/efficiency.js
@@ -1,3 +1,12 @@
+/* 
+Robot efficiency
+Can you write a robot that finishes the delivery task faster than goalOrientedRobot? If you observe that robot’s behavior, what obviously stupid things does it do? How could those be improved?
+
+If you solved the previous exercise, you might want to use your compareRobots function to verify whether you improved the robot.
+
+runRobotAnimation(VillageState.random(), yourRobot, memory);
+*/
+
 function lazyRobot({place, parcels}, route) {
   if (route.length == 0) {
     // Describe a route for every parcel
